fd76d4a416dfdb51a04e5652cf6776a0bf5526e7
romanboisiuk <rboisiuk@lvivsoft.com>
2020-08-25 12:29:55 +0300

    added new app

--- music_one/settings.py
+++ music_one/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'insta_parser',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
