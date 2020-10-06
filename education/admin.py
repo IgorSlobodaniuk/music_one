03377d17941b34374e4cf6bb6a6f1bc53290b452
romanboisiuk <rboisiuk@lvivsoft.com>
2020-10-06 22:12:10 +0300

    remade quest structure

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,79 @@
 from django.contrib import admin
+from education.models.quest_structure import *
 
-# Register your models here.
+
+class EducationLevelAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(EducationLevel, EducationLevelAdmin)
+
+
+class LevelExamAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(LevelExam, LevelExamAdmin)
+
+
+class LevelExamQuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(LevelExamQuestion, LevelExamQuestionAdmin)
+
+
+class LevelExamAnswerVariantAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(LevelExamAnswerVariant, LevelExamAnswerVariantAdmin)
+
+
+class QuestGroupAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(QuestGroup, QuestGroupAdmin)
+
+
+class GroupTestAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GroupTest, GroupTestAdmin)
+
+
+class GroupTestQuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GroupTestQuestion, GroupTestQuestionAdmin)
+
+
+class GroupTestAnswerVariantAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GroupTestAnswerVariant, GroupTestAnswerVariantAdmin)
+
+
+class QuestAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Quest, QuestAdmin)
+
+
+class QuestQuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(QuestQuestion, QuestQuestionAdmin)
+
+
+class QuestAnswerVariantAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(QuestAnswerVariant, QuestAnswerVariantAdmin)
--- education/models/quest_structure.py
+++ education/models/quest_structure.py
@@ -1,53 +1,52 @@
 from django.db import models
 
-from main.models.user_info import MusicOneUser
+# LEVEL_STATUS_CHOISES = (
+#     ('passed', 'passed'),
+#     ('current', 'current')
+# )
 
-
-LEVEL_STATUS_CHOISES = (
-    ('passed', 'passed'),
-    ('current', 'current')
-)
-
-QUEST_CHOISES = (
-    (1, 1),
-    (2, 2),
-    (3, 3),
-    (4, 4),
-)
-
-
+LEVEL_CHOISES = [(i, i) for i in range(1, 10)]
 
 
 class EducationLevel(models.Model):
     name = models.PositiveIntegerField(
-        unique=True
+        unique=True,
+        choices=LEVEL_CHOISES,
     )
     theory = models.TextField()
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class LevelExam(models.Model):
-    name = models.CharField(max_length=500)
     level = models.OneToOneField(
         EducationLevel,
         on_delete=models.PROTECT
     )
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 
-class LevelExamVariant(models.Model):
-    name = models.CharField(max_length=500)
+class LevelExamQuestion(models.Model):
     level = models.OneToOneField(
         LevelExam,
         on_delete=models.PROTECT
     )
+    name = models.CharField(max_length=500)
 
-    question = models.CharField(max_length=500)
-    answer = models.CharField(max_length=500)
+    def __str__(self):
+        return self.name
+
+
+class LevelExamAnswerVariant(models.Model):
+    question = models.ForeignKey(
+        LevelExamQuestion,
+        on_delete=models.PROTECT
+    )
+    name = models.CharField(max_length=500)
     is_correct = models.BooleanField()
 
     def __str__(self):
@@ -55,20 +54,20 @@ class LevelExamVariant(models.Model):
 
 
 class QuestGroup(models.Model):
-    name = models.PositiveIntegerField()
-    level = models.OneToOneField(
+    level = models.ForeignKey(
         EducationLevel,
         on_delete=models.PROTECT
     )
+    name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
 
 
 class Quest(models.Model):
-    name = models.PositiveIntegerField()
+    name = models.CharField(max_length=500)
     number = models.CharField(max_length=500)
-    group = models.OneToOneField(
+    group = models.ForeignKey(
         QuestGroup,
         on_delete=models.PROTECT
     )
@@ -78,38 +77,56 @@ class Quest(models.Model):
 
 
 class GroupTest(models.Model):
-    name = models.PositiveIntegerField()
     group = models.OneToOneField(
         QuestGroup,
         on_delete=models.PROTECT
     )
+    name = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name
 
 
-class GroupTestVariant(models.Model):
-    name = models.CharField(max_length=500)
+class GroupTestQuestion(models.Model):
     group = models.OneToOneField(
         GroupTest,
         on_delete=models.PROTECT
     )
-    question = models.CharField(max_length=500)
-    answer = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class GroupTestAnswerVariant(models.Model):
+    question = models.ForeignKey(
+        GroupTestQuestion,
+        on_delete=models.PROTECT
+    )
+    name = models.CharField(max_length=500)
     is_correct = models.BooleanField()
 
     def __str__(self):
         return self.name
 
 
-class QuestItem(models.Model):
-    name = models.CharField(max_length=500)
-    quest = models.OneToOneField(
+class QuestQuestion(models.Model):
+    quest = models.ForeignKey(
         Quest,
         on_delete=models.PROTECT
     )
-    question = models.CharField(max_length=500)
-    answer = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class QuestAnswerVariant(models.Model):
+    question = models.ForeignKey(
+        QuestQuestion,
+        on_delete=models.PROTECT
+    )
+    name = models.CharField(max_length=500)
     is_correct = models.BooleanField()
 
     def __str__(self):
