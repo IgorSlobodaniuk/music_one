6de669f318d67c09f25edf4bdb16241832a68a56
romanboisiuk <rboisiuk@lvivsoft.com>
2020-11-08 21:57:20 +0200

    added docker and configured

--- music_one/settings.py
+++ music_one/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
